db317708b0843f1255d32dcfc6ad78696ff7f36d
navin21k <96637041+navin21k@users.noreply.github.com>
2022-04-26 19:41:26 -0400

    boo

--- gamedata_collection.py
+++ gamedata_collection.py
@@ -6,7 +6,7 @@ import time
  
 def load_json():
     g = open('matches.json')
-    global matches
+    global match_ids
     matches = json.load(g)
     for item in matches: 
         match_ids = item["match_id"]
@@ -14,47 +14,36 @@ def load_json():
 
 load_json()
 
-# def get_data(): 
-#     global match_data 
-#     match_data = []
-#     first = 0
-#     last = 19
-#     start_time = time.time()
-#     while True:
-#         current_time = time.time()
-#         elapsed_time = current_time - start_time
-#         if elapsed_time> 20:
-#             data = get_match_data(matches[first:last])
-#             match_data.append(data)
-#             first += 20
-#             last =+ 20
-#             elapsed_time = 0
-#             start_time = time.time()
-#         if last >= 20:
-#             break
+def get_data(): 
+    global match_data 
+    match_data = []
+    first = 0
+    last = 19
+    start_time = time.time()
+    while True:
+        current_time = time.time()
+        elapsed_time = current_time - start_time
+        if elapsed_time> 30:
+            data = get_match_data(match_ids[first:last])
+            for items in data: 
+                match_data.append(items)
+            first += 20
+            last =+ 20
+            elapsed_time = 0
+            start_time = time.time()
+        if last >= 40:
+            break
+    return match_data
 
-# pprint(get_data())
+get_data()
 
-# def get_arams(): 
-#     aram_data = []
-#     for items in match_data: 
-#         if items['info']['gameMode'] == 'ARAM':
-#             aram_data.append(items)
-#     return aram_data
+def get_arams(): 
+    aram_data = []
+    for items in match_data: 
+        if items['info']['gameMode'] == 'ARAM':
+            aram_data.append(items)
+    json_object = json.dumps(aram_data, indent = 4)
+    with open("aram_data.json", "w") as outfile: 
+        outfile.write(json_object)
 
-
-
-# def get_aram(players):
-#     aram_matches = []
-#     total_matches = get_matches(players)
-#     total_data = get_match_data(total_matches)
-#     for items in total_data: 
-#         if items['info']['gameMode'] == 'ARAM':
-#             aram_matches.append(items)
-#     return aram_matches
-
-# pprint(get_aram(puuids))
-
-# json_object = json.dumps(aram_matches, indent = 4)
-# with open("aram_data.json", "w") as outfile: 
-#     outfile.write(json_object)
+get_arams()
