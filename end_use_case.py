6cbbe7593800ef26fed4dc2197fce44ecc9f66df
Xavier <151433x@gmail.com>
2022-04-29 19:19:52 -0400

    uploaded dict of champs and champ ids

--- end_use_case.py
+++ end_use_case.py
@@ -1,10 +1,10 @@
-from readline import append_history_file
-from flask import appcontext_popped
+from tkinter.font import names
+from unicodedata import name
 from config import API_KEY1
 from player_collection import riot_apikey
 import json
 import urllib
-
+import pickle
 summoner_name_to_encrypted_api="https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/"
 live_game_finder_api='https://na1.api.riotgames.com/lol/spectator/v4/active-games/by-summoner/'
 def get_json(url):
@@ -17,24 +17,51 @@ def get_json(url):
     response_data = json.loads(response_text)
     return response_data
 
+def loadData():
+    # for reading also binary mode is important
+    picklefile = open('./champion_ids.pickle','rb')     
+    champion_names = pickle.load(picklefile)
+    picklefile.close()
+    return champion_names
+
 def summoner_name_to_encrypted(summoner_name):
     encrypted_summoner_json=get_json(f"{summoner_name_to_encrypted}{summoner_name}?api_key={API_KEY1}")
     return encrypted_summoner_json['id']
-    
-def live_game_finder(name):
+
+list_bois=loadData()
+
+def live_game_finder(name,champs):
     encrypted_id=summoner_name_to_encrypted(name)
     big_live_boi=get_json(f'{live_game_finder_api}{encrypted_id}?api_key={API_KEY1}')
+    
     gamemode=big_live_boi['gameMode']
     team_1=[]
     team_2=[]
     for i in big_live_boi['participants']:
         if i['teamID']=='100':
-            team_1.append(i['championID'])
+            team_1.append(champs[i]['championID'])
+            team_1.append(i)['summonerName']
         else:
-            team_2.append(i['ChampionID'])
+            team_1.append(champs[i]['championID'])
+            team_2.append(i)['summonerName']    
     return team_1,team_2,gamemode
 
+def get_list_champs():
+    names=dict(line.rstrip().split(':', 1) for line in open('champion_ids.txt'))
+    return names
+
+picklefile = open('./champion_ids.pickle','wb')      
+# source, destination
+pickle.dump(get_list_champs(), picklefile)                     
+picklefile.close()
+
+def loadData():
+    # for reading also binary mode is important
+    picklefile = open('./champion_ids.pickle','rb')     
+    champion_names = pickle.load(picklefile)
+    picklefile.close()
+    return champion_names
 
 
 
-print(summoner_name_to_encrypted())
+print(loadData())
